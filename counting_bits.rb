a2265dd5a91444cfe7ec627c2b30028870ed7bf6
brightokike@gmail.com <brightokike@gmail.com>
2020-07-30 15:04:24 +0300

    added coding challenge counting bits

--- counting_bits.rb
+++ counting_bits.rb
@@ -13,3 +13,41 @@ def count_bits(num)
     end
     arr
 end
+
+# SECOND SOLUTION
+# @param {Integer} num
+# @return {Integer[]}
+def count_bits(num)
+    if num == 0
+      return [0]
+    end
+    arr = [0] 
+    for i in 1..num do
+      count = 0
+      n = i
+      while(n>0) do
+        count += n & 1
+        n >>= 1
+        # count += 1 if n % 2 != 0
+        # n /= 2
+      end
+      arr << count
+    end
+
+    arr
+end
+# 0(n^2)
+# 0(n)
+
+# 5
+# 0 1, 2
+# 0 -> 0
+# 1 -> 0 1
+# 2 -> 1 0
+# 3 -> 1 1 
+# 4 -> 1 0 0
+# 5 -> 1 0 1
+
+# [0, 1, 1, 2, 1, 2]
+
+
